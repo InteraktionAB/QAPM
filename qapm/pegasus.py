fa068124912eb6d32041b09c7241a09257badab8
Sreerag M <sreeragibtl@gmail.com>
2022-05-18 15:07:12 +0530

    add HFPegasus

---
+++ qapm/pegasus.py
@@ -0,0 +1,15 @@
+"""
+pegasus
+
+This module contain various Pegasus implementations.
+"""
+
+import abstract
+
+class HFPegasus(abstract.Pegasus):
+
+    """
+    HFPegasus
+
+    Implementation of Pegasus.
+    """
