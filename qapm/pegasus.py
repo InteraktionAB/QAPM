a929313d035f9b0406ddf6d70162ccccd00e7f87
Sreerag M <sreeragibtl@gmail.com>
2022-05-19 14:27:38 +0530

    update test

--- qapm/pegasus.py
+++ qapm/pegasus.py
@@ -28,12 +28,12 @@ class HFPegasus(abstract.Paraphrase):
         """
 
         model_name = 'tuner007/pegasus_paraphrase'
-        torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.tokenizer = PegasusTokenizer.from_pretrained(model_name)
-        self.model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+        self.model = PegasusForConditionalGeneration.from_pretrained(model_name).to(self.torch_device)
 
     def infer(self, input_text, num_return_sequences, num_beams, max_length=60, temperature=1.5):
-        batch = self.tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
+        batch = self.tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(self.torch_device)
         translated = self.model.generate(**batch,max_length=max_length,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=temperature)
         tgt_text = self.tokenizer.batch_decode(translated, skip_special_tokens=True)
         return tgt_text
